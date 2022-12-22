b2f2b007599cc5769c3e6f8bc6c536163014987e
SonicMastr <jaylongowie46@gmail.com>
2022-12-22 01:06:39 -0600

    Add VitaGL renderer path

--- McEngine/src/Engine/Main/main_Vita.cpp
+++ McEngine/src/Engine/Main/main_Vita.cpp
@@ -1,7 +1,11 @@
 #ifdef __vita__
 
+#ifdef PVR
 int _newlib_heap_size_user = 100 * 1024 * 1024;
 unsigned int sceLibcHeapSize = 30 * 1024 * 1024;
+#else
+int _newlib_heap_size_user = 340 * 1024 * 1024;
+#endif
 
 #include "cbase.h"
 
@@ -15,9 +19,11 @@ unsigned int sceLibcHeapSize = 30 * 1024 * 1024;
 #include <SDL2/SDL.h>
 #include <vitasdk.h>
 
+#ifdef PVR
 extern "C" {
 	#include <gpu_es4/psp2_pvr_hint.h>
 }
+#endif
 
 #define MAX_PATH 256
 
@@ -26,41 +32,40 @@ extern int mainSDL(int argc, char *argv[], SDLEnvironment *customSDLEnvironment)
 int main(int argc, char* argv[])
 {
 	int ret = 0;
-
+#ifdef PVR
 	PVRSRV_PSP2_APPHINT hint;
-    char target_path[MAX_PATH];
-
-    scePowerSetArmClockFrequency(444);
+	char target_path[MAX_PATH];
+#endif
+	scePowerSetArmClockFrequency(444);
 	scePowerSetGpuClockFrequency(222);
 
-    /* Disable Back Touchpad to prevent "misclicks" */
-    SDL_setenv("VITA_DISABLE_TOUCH_BACK", "1", 1);
+	/* Disable Back Touchpad to prevent "misclicks" */
+	SDL_setenv("VITA_DISABLE_TOUCH_BACK", "1", 1);
 
-    scePowerSetGpuClockFrequency(222);
+#ifdef PVR
+	/* We need to use some custom hints */
+	SDL_setenv("VITA_PVR_SKIP_INIT", "yeet", 1);
 
-    /* We need to use some custom hints */
-    SDL_setenv("VITA_PVR_SKIP_INIT", "yeet", 1);
+	/* Load Modules */
+	sceKernelLoadStartModule("vs0:sys/external/libfios2.suprx", 0, NULL, 0, NULL, NULL);
+	sceKernelLoadStartModule("vs0:sys/external/libc.suprx", 0, NULL, 0, NULL, NULL);
+	snprintf(target_path, MAX_PATH, "app0:module/%s", "libgpu_es4_ext.suprx");
+	sceKernelLoadStartModule(target_path, 0, NULL, 0, NULL, NULL);
+	snprintf(target_path, MAX_PATH, "app0:module/%s", "libIMGEGL.suprx");
+	sceKernelLoadStartModule(target_path, 0, NULL, 0, NULL, NULL);
 
-    /* Load Modules */
-    sceKernelLoadStartModule("vs0:sys/external/libfios2.suprx", 0, NULL, 0, NULL, NULL);
-    sceKernelLoadStartModule("vs0:sys/external/libc.suprx", 0, NULL, 0, NULL, NULL);
-    snprintf(target_path, MAX_PATH, "app0:module/%s", "libgpu_es4_ext.suprx");
-    sceKernelLoadStartModule(target_path, 0, NULL, 0, NULL, NULL);
-    snprintf(target_path, MAX_PATH, "app0:module/%s", "libIMGEGL.suprx");
-    sceKernelLoadStartModule(target_path, 0, NULL, 0, NULL, NULL);
-
-    /* Set PVR Hints */
-    PVRSRVInitializeAppHint(&hint);
-    snprintf(hint.szGLES1, MAX_PATH, "app0:module/%s", "libGLESv1_CM.suprx");
-    snprintf(hint.szGLES2, MAX_PATH, "app0:module/%s", "libGLESv2.suprx");
-    snprintf(hint.szWindowSystem, MAX_PATH, "app0:module/%s", "libpvrPSP2_WSEGL.suprx");
+	/* Set PVR Hints */
+	PVRSRVInitializeAppHint(&hint);
+	snprintf(hint.szGLES1, MAX_PATH, "app0:module/%s", "libGLESv1_CM.suprx");
+	snprintf(hint.szGLES2, MAX_PATH, "app0:module/%s", "libGLESv2.suprx");
+	snprintf(hint.szWindowSystem, MAX_PATH, "app0:module/%s", "libpvrPSP2_WSEGL.suprx");
 	//hint.bDumpProfileData = 1;
 	//hint.bDisableMetricsOutput = 0;
 	//hint.ui32ProfileStartFrame = 40;
 	//hint.ui32ProfileEndFrame = 70;
-    hint.ui32SwTexOpCleanupDelay = 16000; // Set to 16 milliseconds to prevent a pool of unfreed memory
-    PVRSRVCreateVirtualAppHint(&hint);
-	
+	hint.ui32SwTexOpCleanupDelay = 16000; // Set to 16 milliseconds to prevent a pool of unfreed memory
+	PVRSRVCreateVirtualAppHint(&hint);
+#endif	
 
 	ret = mainSDL(argc, argv, new VitaSDLEnvironment());
 	
